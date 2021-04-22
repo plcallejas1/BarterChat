e4fdde6d05f23f7d0228ddc10ea531af9b3888ac
Paul Callejas <pl.callejas@icloud.com>
2021-04-22 00:48:33 -0500

    Initial Commit

---
+++ Discussion Board/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Discussion Board
+//
+//  Created by admin on 4/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Discussion Board/Discussion_BoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Discussion_BoardApp.swift
+//  Discussion Board
+//
+//  Created by admin on 4/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Discussion_BoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
